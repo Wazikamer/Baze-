703487cc971fccc6cff96d85b6948c167222dcb1
Oleksii Maslov <wz131h@gmail.com>
2023-02-19 12:43:58 +0200

    parallax

--- project 4/script.js
+++ project 4/script.js
@@ -1,3 +1,53 @@
+window.onload = function () {
+  const parallax = document.querySelector(".parallax");
+  const content = document.querySelector(".parallax__container");
+  const clouds = document.querySelector(".images-parallax__clouds");
+  const city = document.querySelector(".images-parallax__city");
+  const human = document.querySelector(".images-parallax__human");
+  //кофіієнти
+  const forClouds = 40;
+  const forCity = 20;
+  const forHuman = 10;
+
+  //швидкість
+  const speed = 0.05;
+  //змінні
+  let positionX = 0,
+    positionY = 0;
+  let coordXprocent = 0,
+    coordYprocent = 0;
+
+  function setMouseParallaxStyle() {
+    const distX = coordXprocent - positionX;
+    const distY = coordYprocent - positionY;
+
+    positionX = positionX + distX * speed;
+    positionY = positionY + distY * speed;
+
+    clouds.style.cssText =
+      "transform: franslate(${positionX / forClouds}%, ${positionY / forClouds}%)";
+    city.style.cssText =
+      "transform: franslate(${positionX / forCity}%, ${positionY / forCity}%)";
+    human.style.cssText =
+      "transform: franslate(${positionX / forHuman}%, ${positionY / forHuman}%)";
+
+    requestAnimationFrame(setMouseParallaxStyle);
+  }
+
+  setMouseParallaxStyle();
+
+  parallax.addEventListener("mousemove", function (e) {
+    const parallaxWidth = parallax.offsetWidth;
+    const parallaxHeight = parallax.offsetHeight;
+
+    const coordX = e.pageX - parallaxWidth / 2;
+    const coordY = e.pageY - parallaxHeight / 2;
+
+    coordXprocent = (coordX / parallaxWidth) * 100;
+    coordYprocent = (coordY / parallaxHeight) * 100;
+  });
+};
+
 const sliderImgs = document.querySelectorAll(".slider__img"),
   sliderLine = document.querySelector(".slider__line"),
   sliderDots = document.querySelectorAll(".slider__dot"),
